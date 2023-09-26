0f9406b1d17ead1cba972b4e1ab92b0d385aa201
Robyn Lam <osarobyn@gmail.com>
2023-09-26 11:21:19 -0700

    css

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -14,6 +14,8 @@
 
 
 <script>
+
+
 import RecipeCard from './components/RecipeCard.vue'
 
 export default {
@@ -30,9 +32,15 @@ export default {
 </script>
 
 <style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Averia+Serif+Libre:ital,wght@0,300;1,300&family=Caprasimo&family=DM+Sans:opsz,wght@9..40,200;9..40,300;9..40,500&family=Lexend:wght@200;300&family=Montserrat:wght@300;400&display=swap');
+
 
 #app{
-  background: rgb(150, 111, 92);
+  background: #fae7dd;
+  /* background-image: url('https://media.istockphoto.com/id/1531971918/photo/colour-old-concrete-wall-texture-background-close-up-retro-plain-cream-color-cement-wall.webp?b=1&s=170667a&w=0&k=20&c=d9Rw-za44oCqaTBT7ayhHocKmGPh-AOA9bTVxqytpRs='); */
+  background-size: cover;
+  
+
 }
 
 
--- vue/src/components/RecipeDetail.vue
+++ vue/src/components/RecipeDetail.vue
@@ -1,25 +1,212 @@
 <template>
   <div class="container">
+    <h3>
+      Recipe Name
+      <!-- {{recipe.name}} -->
+    </h3>
 
-    <h3>Lorem, ipsum dolor.</h3>
-   <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae iusto aspernatur dolorem dolores? Distinctio, assumenda omnis! Aut magnam laborum vel.
-</p>
+    <section class="servings">
+      <!-- {{recipe.servingsize}} | {{recipe.cookTime}} | {{recipe.etc}}  -->
 
+      3 servings | prep time: 30 min | total time: 1 hour
+    </section>
+
+    <section class="description">
+    <p>
+    
+      <!-- {{recipe.description}} -->
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae iusto
+      aspernatur dolorem dolores? Distinctio, assumenda omnis! Aut magnam
+      laborum vel.
+    </p>
+</section>
+    <div class="ingredients-img-container">
+      <div class="ingredients">
+        <!-- where ID = XYZ v-for Data.Ingredients -->
+        <ul>
+          <li class="ingredient">13 seashells</li>
+          <li class="ingredient">2 friends</li>
+          <li class="ingredient">1/4 tbs green tea</li>
+          <li class="ingredient">13 small rocks</li>
+          <li class="ingredient">1lb sugar</li>
+          <li class="ingredient">2 turtles</li>
+          <li class="ingredient">1/4 tbs green tea</li>
+          
+          <li class="ingredient">13 small rocks</li>
+          <li class="ingredient">1lb flour</li>
+          <li class="ingredient">2 friends</li>
+          <li class="ingredient">1/4 tbs green tea</li>
+          <li class="ingredient">13 small rocks</li>
+          <li class="ingredient">1lb sugar</li>
+        </ul>
+      </div>
+
+      <div class="recipeImage">
+        <!-- {{recipe.imgUrl}} -->
+
+        <img
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT_xdKg2OssMWPE-SqkgOduIZDcEykLm6N6_hdmsJfl&s"
+        />
+      </div>
+    </div>
+
+    <button class="steps-btn">
+      Click Here to Follow the Instructions Step by Step
+    </button>
+
+    <div class="instructions">
+      <h2>Instructions:</h2>
+      <ol>
+        <li class="steps">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore,
+          dolores!
+        </li>
+        <li class="steps">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex iusto,
+          magnam aut iste amet rerum.
+        </li>
+        <li class="steps">Lorem, ipsum dolor. Lorem ipsum dolor sit amet.</li>
+        <li class="steps">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed optio
+          enim velit accusantium quo, magnam neque aut tempore omnis amet
+          necessitatibus provident, maxime dicta? Eos aliquid provident, iste
+          quos quam corrupti harum ducimus temporibus? Quam perferendis nobis
+          architecto nulla! Quis, quisquam totam cupiditate quos quae nobis eius
+          eos provident voluptatum, fugit dignissimos delectus dolorum quam
+          itaque aperiam distinctio culpa perferendis?
+        </li>
+
+        <li class="steps">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur,
+          provident quasi asperiores laborum odit ducimus quod explicabo aliquid
+          eaque, recusandae, corporis quibusdam sequi incidunt ex?
+        </li>
+      </ol>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'recipeDetail',
-  props:{
-
-  },
-  components: {
-    
-  }
-}
+  name: "recipeDetail",
+  props: {},
+  components: {},
+};
 </script>
 
 <style>
+/*
+font-family: 'Averia Serif Libre', cursive;
+font-family: 'Caprasimo', cursive;
+font-family: 'DM Sans', sans-serif;
+font-family: 'Lexend', sans-serif;
+font-family: 'Montserrat', sans-serif;
+*/
 
+.container {
+  overflow: auto;
+  display: flex;
+  flex-direction: column;
+}
+h2 {
+  font-size: 1.4rem;
+  border-bottom: 3px solid #444;
+}
+h3 {
+  font-family: "Averia Serif Libre", cursive;
+  text-transform: capitalize;
+  letter-spacing: 1px;
+  padding-bottom: 0.3rem;
+  font-size: 1.5rem;
+  border-bottom: 10px solid #55695659;
+}
+.servings {
+  position: relative;
+  text-align: right;
+  font-family: "Montserrat";
+  color: #555;
+  font-size: 0.8em;
+  margin-top: -1rem;
+}
+
+.description{
+  text-align: justify;
+  padding: 0rem .5rem;
+font-family: 'DM Sans', sans-serif;
+font-weight: 300;
+}
+p {
+
+}
+
+.ingredients {
+  font-size: 1rem;
+  font-family: "Montserrat", sans-serif;
+  overflow: auto;
+}
+.ingredients-img-container {
+  position: relative;
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  padding-bottom: 1rem;
+}
+.recipeImage img {
+  width: auto;
+  padding-top: 10px;
+  height: 100%;
+  max-height: 260px;
+
+}
+
+.steps-btn {
+  font-family: "Montserrat", sans-serif;
+  background: rgb(57, 82, 76);
+  color: #fff;
+  border-radius: 30px;
+  border: 1px solid #333;
+  font-size: 1rem;
+  padding: 1.4%;
+  float: right;
+  margin: 0rem 0rem;
+  transition: ease-in .4s;
+}
+
+.steps-btn:hover {
+  background: #883410;
+  /* background-image: url('https://t4.ftcdn.net/jpg/02/87/02/71/240_F_287027124_R2xwyI2l2ncGKFqy6JF3Izaue1xEWfMn.jpg'); */
+  transition: ease-out .3s;
+}
+
+.instructions {
+  font-size: .9rem;
+  padding-right: 1.3rem;
+  line-height: 2rem;
+font-family: 'Lexend', sans-serif;
+
+}
+
+.instructions ol li:nth-child(odd) {
+  color: rgb(170, 84, 84);
+}
+::-webkit-scrollbar {
+  width: 10px;
+  scroll-padding-right: 3rem;
+}
+
+/* Track */
+::-webkit-scrollbar-track {
+  background: rgb(224, 222, 207);
+}
+
+/* Handle */
+::-webkit-scrollbar-thumb {
+  background: #304b39e7;
+  border-radius: 5px;
+  scroll-padding-right: auto;
+}
+
+/* Handle on hover */
+::-webkit-scrollbar-thumb:hover {
+  background: rgb(111, 187, 130);
+}
 </style>
