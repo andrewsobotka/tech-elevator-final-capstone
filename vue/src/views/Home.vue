ce76c0c4064dbf7cdcc4094651c7e4eca2d79f05
Jackie Lee <jlee3545@Gmail.com>
2023-09-26 19:30:50 -0400

    fix

--- vue/src/views/Home.vue
+++ vue/src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="home">
-    <h1>Home</h1>
+    <h1>Welcome, {{$store.state.user.username}}!</h1>
     <p>If you are seeing this, you are authenticated.</p>
   </div>
 </template>
