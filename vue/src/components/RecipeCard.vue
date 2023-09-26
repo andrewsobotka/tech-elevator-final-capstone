0b38f49f196f0dd374e0266fe2011d14909d2b8b
Robyn Lam <osarobyn@gmail.com>
2023-09-26 08:47:04 -0700

    ?

--- vue/src/components/RecipeCard.vue
+++ vue/src/components/RecipeCard.vue
@@ -1,14 +1,38 @@
 <template>
-  <div>
+  <div class="cardContainer">
+   <recipeDetail>
+
   </div>
+
+
 </template>
 
 <script>
+import recipeDetail from '../src/components/RecipeDetail.vue'
+
 export default {
+    name: 'recipeCard',
+  props:{
+
+  },
+  components: {
+    recipeDetail,
+    
+  }
 
 }
 </script>
 
 <style>
+
+.cardContainer{
+  background: #fff;
+  border-radius: 20px;
+  border: 1px #333 solid;
+
+}
+
+
+
 
 </style>
