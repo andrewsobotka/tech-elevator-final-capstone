0a364b722447835d00d128312a59c8746422437c
Robyn Lam <osarobyn@gmail.com>
2023-09-27 13:56:06 -0700

    styles readjustment

--- vue/src/main.js
+++ vue/src/main.js
@@ -3,6 +3,9 @@ import App from './App.vue'
 import router from './router/index'
 import store from './store/index'
 import axios from 'axios'
+import './main/mainstyles.css'
+
+
 
 Vue.config.productionTip = false
 
