e1d8dbd969db2a50028e6b850087934ab1302251
Jackie Lee <jlee3545@Gmail.com>
2023-10-01 19:13:22 -0400

    merge

--- java/src/main/java/com/techelevator/dao/JdbcRecipeDao.java
+++ java/src/main/java/com/techelevator/dao/JdbcRecipeDao.java
@@ -1,16 +1,15 @@
 package com.techelevator.dao;
 
 import com.techelevator.exception.DaoException;
+import com.techelevator.model.Ingredient;
 import com.techelevator.model.Recipe;
-import org.springframework.dao.DataAccessException;
+import com.techelevator.model.Tag;
 import org.springframework.dao.DataIntegrityViolationException;
-import org.springframework.data.jdbc.repository.query.Modifying;
 import org.springframework.jdbc.CannotGetJdbcConnectionException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.support.rowset.SqlRowSet;
 import org.springframework.stereotype.Component;
 
-import java.security.Principal;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -26,7 +25,7 @@ public class JdbcRecipeDao implements RecipeDao {
     @Override
     public List<Recipe> getListOfRecipes() {                //Displayed on Explore Recipes page (whole "library" of recipes)
         List<Recipe> recipes = new ArrayList<>();
-        String sql = "select * from recipes;";
+        String sql = "select * from recipes";
 
         SqlRowSet results = jdbcTemplate.queryForRowSet(sql);
         while (results.next()) {
@@ -58,50 +57,19 @@ public class JdbcRecipeDao implements RecipeDao {
     }
 
     @Override
-<<<<<<< HEAD
-    public Recipe createNewRecipe(Recipe recipe) {
-
-        Recipe newRecipe = null;
-//        String sql_users = "SELECT user_id FROM users WHERE username = ?";
-
-        String sql_recipes = "INSERT INTO recipes (creator_id, recipe_name, image_url, description, serving_size, keywords) " +
-                "VALUES (?, ?, ?, ?, ?, ?) RETURNING recipe_id;";
-
-        String sql_users_recipes = "INSERT INTO users_recipes (user_id, recipe_id) " +
-                "VALUES (?, ?);";
-
-        try {
-
-//            int user_id = jdbcTemplate.queryForObject(sql_users, int.class, username);
-            int newRecipeId = jdbcTemplate.queryForObject(sql_recipes, int.class,
-                    1, recipe.getRecipeName(), recipe.getImgUrl(), recipe.getDescription(), recipe.getServingSize(), recipe.getKeywords());
-            jdbcTemplate.queryForObject(sql_users_recipes, int.class, 1, newRecipeId);
-
-            newRecipe = getRecipeByRecipeId(newRecipeId);
-            newRecipe.setCreatorId(1);
-
-        } catch (CannotGetJdbcConnectionException e) {
-            throw new DaoException("Unable to connect to server or database", e);
-        } catch (DataIntegrityViolationException e) {
-            throw new DaoException("Data integrity violation", e);
-        }
-        return newRecipe;
-    }
-
-    @Override
     public List<Recipe> getFeaturedRecipesByRecipeId(int recipe_id) {           //Display Top3-Featured Recipes
 
-=======
-    public List<Recipe> getFeaturedRecipesByRecipeId() {           //Display Top3-Featured Recipes
->>>>>>> bc60f87e77c324969b937eac362c1875f5bbcc07
         List<Recipe> recipes = new ArrayList<>();
-        String sql = "SELECT * FROM recipes WHERE is_featured = true;";
-        SqlRowSet results = jdbcTemplate.queryForRowSet(sql);
+        String sql = "select * from recipes where recipe_id = ? ";
+
+        SqlRowSet results = jdbcTemplate.queryForRowSet(sql, recipe_id);
         while (results.next()) {
             Recipe recipe = mapRowToRecipe(results);
             recipes.add(recipe);
         }
+
         return recipes;
+
     }
 
     @Override
@@ -136,25 +104,6 @@ public class JdbcRecipeDao implements RecipeDao {
         }
 
         return recipes;
-    }
-
-    @Override
-    public Integer createRecipe(Recipe recipe, Principal principal) {
-        // Get the User ID by Username
-        String sql_user_id = "SELECT user_id FROM users " +
-                             "WHERE username = ?;";
-        // Create New Recipe
-        String sql_create_recipe = "INSERT INTO recipes (creator_id, recipe_name, image_url, description, serving_size, keywords, is_published, is_featured) " +
-                     "VALUES (?,?,?,?,?,?,?,?) " +
-                     "RETURNING recipe_id;";
-        Integer recipeId, userId = 0;
-        try {
-            userId = jdbcTemplate.queryForObject(sql_user_id, Integer.class, principal.getName());
-            recipeId = jdbcTemplate.queryForObject(sql_create_recipe, Integer.class, userId, recipe.getRecipeName(), recipe.getImgUrl(), recipe.getDescription(), recipe.getServingSize(), recipe.getKeywords(), recipe.isPublished(), recipe.isFeatured());
-        } catch (DataAccessException e){
-            throw new DataAccessException(e.toString()) {};
-        }
-        return recipeId;
     }
 
     private Recipe mapRowToRecipe(SqlRowSet rs) {
