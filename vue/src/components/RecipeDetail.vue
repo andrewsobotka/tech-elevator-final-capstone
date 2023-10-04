38e609c72f4356eae77f7791adcac846f005a344
Jackie Lee <jlee3545@Gmail.com>
2023-10-04 16:20:45 -0400

    buttons

--- vue/src/components/RecipeDetail.vue
+++ vue/src/components/RecipeDetail.vue
@@ -360,4 +360,24 @@ ul li{
 #featureButton:hover{
   background: gold;
 }
+
+.popup{
+  position:fixed;
+  top:0;
+  left:0;
+  right:0;
+  bottom:0;
+  z-index: 99;
+  background-color: rgba(0,0,0,0.2);
+
+  display: flex;
+  align-items:center;
+  justify-content: center;
+
+  
+}
+.popup-inner{
+    background: #fff;
+    padding:32px;
+  }
 </style>  
---
+++ vue/src/components/stepsPopup.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class = "popup">
+      <div class = "popup-inner">
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
